be9aa2f8c104b06fb64d3832bb32a71c1cbda6b7
Stine Olsen <stine.olsen@knowit.no>
2024-05-08 10:42:20 +0200

    Fix iframe size
    Iframe hight cannot be in rem needs to be in px

--- src/pageComponents/TicketPageContent.tsx
+++ src/pageComponents/TicketPageContent.tsx
@@ -7,7 +7,7 @@ export const TicketPageContent = () => {
       <iframe
         src='https://ticketco.events/no/nb/widgets/organizers/104/events?sections%5B%5D=title&amp;sections%5B%5D=datetime&amp;sections%5B%5D=tickets&amp;sections%5B%5D=location&amp;sections%5B%5D=contact_organizer&amp;sections%5B%5D=description&amp;tags=&amp;flush=true'
         width='100%'
-        height='50rem'
+        height='800px'
         frameBorder='0'
         scrolling='auto'
         title='Frame section containing the shows and tickets available'

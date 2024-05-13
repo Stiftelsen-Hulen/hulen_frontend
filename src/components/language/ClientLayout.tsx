6e827597cd636ca466c00ffd795e14f00a1e6bd4
Stine Olsen <stine.olsen@knowit.no>
2024-05-13 10:41:01 +0200

    Style space between main and footer

--- src/components/language/ClientLayout.tsx
+++ src/components/language/ClientLayout.tsx
@@ -30,7 +30,7 @@ export const ClientLayout = ({
         height='100%'
         width='100%'>
         <NavigationBar navbarElements={headerData} />
-        <Box component={"main"} marginTop={{ xs: '2rem', md: '4rem' }} width={'100%'}>{children}</Box>
+        <Box component={"main"} marginTop={{ xs: '2rem', md: '4rem' }} width={'100%'} marginBottom={'1rem'}>{children}</Box>
         <Footer footerElements={footerData} />
       </Stack>
     </LanguageProvider>

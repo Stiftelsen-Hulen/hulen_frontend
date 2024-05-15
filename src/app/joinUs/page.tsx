714e345eff3a665d0af319aeba825096c1934561
Stine Olsen <stine.olsen@knowit.no>
2024-05-15 10:42:19 +0200

    Remove unused import

--- src/app/joinUs/page.tsx
+++ src/app/joinUs/page.tsx
@@ -1,6 +1,5 @@
 import { JoinUsPageContent } from '@/pageComponents/joinUsPage'
 import { getJoinUsPageContent } from '@/util/sanity'
-import { DEFAULT_LAYOUT_MAXWIDTH } from '@/configs/constants'
 import { Stack } from '@mui/material'
 
 export default async function JoinUsPage() {

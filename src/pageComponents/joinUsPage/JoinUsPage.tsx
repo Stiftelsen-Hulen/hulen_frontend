957d1ac128296ccb6fefc9cd6bb31c45a0c55d5c
Stine Olsen <stine.olsen@knowit.no>
2024-05-21 09:09:32 +0200

    Refactor Position section
    Move intro, buttons and description into its own position section

--- src/pageComponents/joinUsPage/JoinUsPage.tsx
+++ src/pageComponents/joinUsPage/JoinUsPage.tsx
@@ -5,12 +5,10 @@ import { useLanguage } from '@/util/LanguageContext/LanguageContext'
 import { Box, Button, Stack, Typography } from '@mui/material'
 import { PortableText } from '@portabletext/react'
 import { scrollToSection } from '@/util/helpers'
-import { PositionButtons } from './components/position/PositionButtons'
-import { PositionEntry } from './components/position/PositionEntry'
 import { BenefitsSection } from './components/sections/BenefitsSection'
 import { JoinUsSection } from './components/sections/JoinUsSection'
-import Image from 'next/image'
 import { DEFAULT_LAYOUT_MAXWIDTH } from '@/configs/constants'
+import { PositionSection } from './components/sections/PositionSection'
 
 export const JoinUsPageContent = ({ content }: { content: JoinUsSanityContent }) => {
   const { language } = useLanguage()
@@ -46,36 +44,13 @@ export const JoinUsPageContent = ({ content }: { content: JoinUsSanityContent })
           }
 
         </Box>
-        <Box width={{ xs: '100%', md: '50%' }} alignSelf={'start'} textAlign={{ xs: 'center', md: 'left' }}>
+        <Box sx={{ width: { xs: '100%', md: '50%' }, alignSelf: 'start', textAlign: { xs: 'center', md: 'left' } }}>
           <PortableText value={content.ingress[language]} />
         </Box>
-
-        <Stack justifyContent={'center'}>
-          <Box
-            sx={{
-              width: content.positionPreface.descImage.asset.metadata.dimensions.width ?? '6.25rem',
-              height: content.positionPreface.descImage.asset.metadata.dimensions.height ?? '6.25rem',
-            }}
-          >
-            <Image
-              layout='responsive'
-              src={content.positionPreface.descImage.asset.url ?? ''}
-              alt={''}
-              width={content.positionPreface.descImage.asset.metadata.dimensions.width ?? '6.25rem'}
-              height={content.positionPreface.descImage.asset.metadata.dimensions.height ?? '6.25rem'}
-            />
-          </Box>
-        </Stack>
-
-        <PositionButtons positions={content.positions} introductoryText={content.positionPreface} />
-        <Stack gap='2rem' margin='2rem 0rem' id='positions' width={'100%'}>
-          {content.positions.map((position, index) => (
-            <PositionEntry position={position} key={index} />
-          ))}
-        </Stack>
+        <PositionSection content={content.positionPreface} positions={content.positions} />
         <BenefitsSection content={content.benefitsSection} />
-
       </Stack>
+
       <JoinUsSection content={content.joinSection} />
     </Stack >
   )
--- src/pageComponents/joinUsPage/components/position/PositionButtons.tsx
+++ src/pageComponents/joinUsPage/components/position/PositionButtons.tsx
@@ -1,26 +1,16 @@
 import { Position, PositionShiftType } from '@/types/sanity/joinUsPage/position'
-import { Stack, Typography } from '@mui/material'
+import { Stack } from '@mui/material'
 import { useMemo } from 'react'
 import { ButtonRow } from './ButtonRow'
-import { PositionDescriptionSection } from '@/types/sanity/joinUsPage'
-import { useLanguage } from '@/util/LanguageContext/LanguageContext'
-import { PortableText } from '@portabletext/react'
 
 /**
  * PositionButtons generates buttons for different job positions, and organizing them based on their shift type.
- * It also displays introductory text related to the positions.
- * @param param0 
- * @returns 
  */
 export const PositionButtons = ({
-  positions,
-  introductoryText,
+  positions
 }: {
   positions: Position[]
-  introductoryText: PositionDescriptionSection
 }) => {
-  const { language } = useLanguage()
-
   const organized = useMemo(() => {
     const byType: Record<PositionShiftType, Position[]> = { night_shift: [], outside_regular: [] }
 
@@ -39,11 +29,6 @@ export const PositionButtons = ({
 
   return (
     <Stack alignItems={'center'} gap='2rem'>
-      <Typography variant='h3'>{introductoryText.header[language]}</Typography>
-      <Stack>
-        <PortableText value={introductoryText.content[language]} />
-      </Stack>
-
       {Object.entries(organized).map(([key, value]) => (
         <ButtonRow key={key} title={key} positions={value} />
       ))}

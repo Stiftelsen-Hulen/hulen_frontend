a9b8c7f65c9298161449be503b9c25a67587da58
Henriette Ameln <henriette.ameln@knowit.no>
2024-09-06 13:22:17 +0200

    Merge two stacks to one

--- src/pageComponents/infoPage/GuardianSection.tsx
+++ src/pageComponents/infoPage/GuardianSection.tsx
@@ -11,41 +11,41 @@ export const GuardianSection = ({ content }: { content: GuardianInfoPageContent 
   const { language } = useLanguage()
 
   return (
-    <Stack sx={{ alignItems: 'center', padding: '2rem', width: '100%' }}>
-      <Stack
-        sx={{
-          width: '100%',
-          maxWidth: DEFAULT_LAYOUT_MAXWIDTH,
-          gap: '2rem',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Box sx={{ width: '100%', alignSelf: 'start', textAlign: 'center' }}>
-          <Typography variant='h1' fontWeight={700} width={'100%'}>
-            {content.header[language]}
-          </Typography>
-          <HulenPortableText genericSanityPageProps={content.intro} />
+    <Stack
+      sx={{
+        width: '100%',
+        mx:'auto',
+        padding: '2rem',
+        maxWidth: DEFAULT_LAYOUT_MAXWIDTH,
+        gap: '2rem',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <Box sx={{ width: '100%', alignSelf: 'start', textAlign: 'center' }}>
+        <Typography variant='h1' fontWeight={700} width={'100%'}>
+          {content.header[language]}
+        </Typography>
+        <HulenPortableText genericSanityPageProps={content.intro} />
 
-          <Stack sx={{ flexDirection: { xs: 'column', sm: 'row' }, paddingTop: '2rem' }} gap={5}>
-            <SanityImageComponent imageData={content.guardianImage} width={500} />
-            <Stack
-              sx={{
-                flexDirection: 'column',
-                textAlign: 'left',
-                '& a': { color: 'white' },
-                width: '100%',
-              }}
-            >
-              <Typography variant='h4' fontWeight={700} width={'100%'} textAlign={'center'}>
-                {content.subHeading[language]}
-              </Typography>
-              <HulenPortableText genericSanityPageProps={content.description} />
-            </Stack>
+        <Stack sx={{ flexDirection: { xs: 'column', sm: 'row' }, paddingTop: '2rem' }} gap={5}>
+          <SanityImageComponent imageData={content.guardianImage} width={500} />
+          <Stack
+            sx={{
+              flexDirection: 'column',
+              textAlign: 'left',
+              '& a': { color: 'white' },
+              width: '100%',
+            }}
+          >
+            <Typography variant='h4' fontWeight={700} width={'100%'} textAlign={'center'}>
+              {content.subHeading[language]}
+            </Typography>
+            <HulenPortableText genericSanityPageProps={content.description} />
           </Stack>
-        </Box>
-      </Stack>
+        </Stack>
+      </Box>
     </Stack>
   )
 }

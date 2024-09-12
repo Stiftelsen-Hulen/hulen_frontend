dae8cff85c0dfe804a6a901836a8200e200514cd
Henriette Ameln <henriette.ameln@knowit.no>
2024-09-12 10:10:49 +0200

    Add and edit comments in utils folder.

--- src/util/helpers/scrollToSection.ts
+++ src/util/helpers/scrollToSection.ts
@@ -1,3 +1,6 @@
+/**
+ * Scrolls the page to a specific section identified by the elementId
+ */
 export function scrollToSection(elementId: string) {
   const matchingElement = document.getElementById(elementId)
   if (matchingElement) {

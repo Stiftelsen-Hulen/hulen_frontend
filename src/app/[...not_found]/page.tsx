2721c6769632620c6cb86808cdf39e5910dd99d7
nikolaihg <nikolai.gangstoe@gmail.com>
2025-01-17 11:39:57 +0100

    wrong (125 @ notfound)

--- src/app/[...not_found]/page.tsx
+++ src/app/[...not_found]/page.tsx
@@ -11,4 +11,4 @@ export default async function Not_Found() {
  * Next js use implicit configuration of pages. By exporting a variable named revalidate, we have now switched to incremental static regeneration (ISR)
  * This means that the page will be rebuilt if a request comes in, and the page is more than two minutes old
  */
-export const revalidate = 125
+export const revalidate = 120

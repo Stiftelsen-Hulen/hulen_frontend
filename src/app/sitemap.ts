9488c9a7feef9fc552eb61f9b081dea44382fad0
Halvor Brunt <87039605+Halvor0808@users.noreply.github.com>
2025-05-25 00:30:31 +0200

    44- dynamic sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,30 +1,42 @@
 import type { MetadataRoute } from 'next'
+import path from 'path'
+import fs from 'fs'
 
-export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    {
-      url: 'https://hulen.no',
-    },
-    {
-      url: 'https://www.hulen.no/frivillig',
-    },
-    {
-      url: 'https://www.hulen.no/billetter',
-    },
-    {
-      url: 'https://www.hulen.no/info/tech',
-    },
-    {
-      url: 'https://www.hulen.no/info/verge',
-    },
-    {
-      url: 'https://www.hulen.no/info/tilgjengelighet',
-    },
-    {
-      url: 'https://www.hulen.no/om',
-    },
-    {
-      url: 'https://www.hulen.no/kontakt',
-    },
-  ]
+const baseUrl = process.env.SITE_URL || 'http:localhost:3000'
+const baseDir = 'src/app'
+export const revalidate = 3600 // revalidate at most every hour
+
+async function getRoutes(): Promise<MetadataRoute.Sitemap> {
+  const fullPath = path.join(process.cwd(), baseDir)
+  const entries = fs.readdirSync(fullPath, { withFileTypes: true, recursive: true })
+  const routes: string[] = ['/']
+
+  entries.forEach((entry) => {
+    if (entry.isDirectory() && entryHasPageFile(entry)) {
+      routes.push(`/${path.relative(fullPath, entry.path)}/${entry.name}`)
+    }
+  })
+
+  return routes.map((route) => ({
+    url: `${baseUrl}${route}`,
+    // lastModified: new Date(),
+    changeFrequency: 'monthly',
+    // priority: 1.0,
+  }))
+}
+
+/**
+ * Checks if the entry (directory / symbolic link) has a 'page.tsx' file.
+ * @param entry file-system entry
+ * @returns whether entry has 'page.tsx' file.
+ */
+function entryHasPageFile(entry: fs.Dirent): boolean {
+  const directories = fs.readdirSync(path.join(entry.path, entry.name), { withFileTypes: true })
+  const pageFile = directories.find((f) => f.name == 'page.tsx')
+
+  return pageFile !== undefined
+}
+
+export default function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return getRoutes()
 }

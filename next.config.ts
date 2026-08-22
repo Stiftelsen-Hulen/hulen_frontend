165927df16c99220dfa847a8a2598a2d285bcc67
Nikolai Gangstø <nikolai.gangsto@protonmail.com>
2026-08-22 23:29:42 +0200

    fix. actual redirect tilgjengelighet to accessibility

--- next.config.ts
+++ next.config.ts
@@ -51,24 +51,24 @@ const nextConfig: NextConfig = {
       },
       {
         source: '/companion',
-        destination: '/info/ledsagerordning',
-        permanent: true,
-        basePath: false,
-      },
-      {
-        source: 'info/availability',
         destination: '/info/tilgjengelighet',
         permanent: true,
         basePath: false,
       },
       {
-        source: '/companion',
-        destination: '/info/ledsagerordning',
+        source: '/info/companion',
+        destination: '/info/tilgjengelighet',
         permanent: true,
         basePath: false,
       },
       {
         source: '/accessibility',
+        destination: '/info/tilgjengelighet',
+        permanent: true,
+        basePath: false,
+      },
+      {
+        source: '/info/accessibility',
         destination: '/info/tilgjengelighet',
         permanent: true,
         basePath: false,
